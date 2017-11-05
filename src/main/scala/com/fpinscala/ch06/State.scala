b87cda2eb0a686d0bc0ecbef07794b906fd0664e
wqlin <wqlin0@gmail.com>
2017-11-05 15:44:39 +0800

    One more exercise to implement

--- src/main/scala/com/fpinscala/ch06/State.scala
+++ src/main/scala/com/fpinscala/ch06/State.scala
@@ -71,6 +71,18 @@ object RNG {
     }
   }
 
+  //Tail recursive solution
+  def intsTailRec(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def loop(count: Int, rng: RNG, ls: List[Int]): (List[Int], RNG) =
+      if (count == 0) (ls, rng)
+      else {
+        val (nextInt, nextRNG) = rng.nextInt
+        loop(count - 1, nextRNG, nextInt :: ls)
+      }
+
+    loop(count, rng, Nil)
+  }
+
   //Exercise 6.5
   def doubleViaMap(rng: RNG): Rand[Double] =
     map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
@@ -83,7 +95,6 @@ object RNG {
       (f(a, b), rng2)
     }
 
-
   def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
     map2(ra, rb)((_, _))
 
@@ -95,31 +106,102 @@ object RNG {
 
   //Exercise 6.7
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
-    rng => fs.foldRight((List.empty[A], rng)) {
-      case ((f, (ls, r))) =>
-        val (i, nextRNG) = f(r)
-        (i :: ls, nextRNG)
-    }
+    fs.foldRight(unit(Nil: List[A]))((a, b) => map2(a, b)(_ :: _))
+
+  //Trivial implementation
+  //  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+  //    rng => fs.foldRight((List.empty[A], rng)) {
+  //      case ((f, (ls, r))) =>
+  //        val (i, nextRNG) = f(r)
+  //        (i :: ls, nextRNG)
+  //    }
 
   def intsViaSequence(count: Int): Rand[List[Int]] =
     sequence(List.fill(count)(int))
 
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    rng =>
+      val (i, rng2) = nonNegativeInt(rng)
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0)
+        (mod, rng2)
+      else
+        nonNegativeLessThan(n)(rng2)
+  }
+
   //Exercise 6.8
-  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
-    rng => {
-      val (i, r) = f(rng)
-      g(i)(r)
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, r) = f(rng)
+    g(a)(r)
+  }
+
+  def nonNegativeLessThanViaFlatMap(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0)
+        unit(mod)
+      else
+        nonNegativeLessThanViaFlatMap(n)
     }
 
-  def nonNegativeLessThan(n: Int): Rand[Int] = ???
-
-
   //Exercise 6.9
-  def mapViaFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] = ???
+  def mapViaFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
 
-  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => flatMap(rb)(b => unit(f(a, b))))
+
+  //Instead, We could also use placeholder to replace unit function
+  def mapViaFlatMap1[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => (f(a), _))
+
+  def map2ViaFlatMap1[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => flatMap(rb)(b => (f(a, b), _)))
 }
 
-//Exercise 6.10
+import State._
+
+//S represents states, A stands for computed value
+case class State[S, +A](run: S => (A, S)) {
+
+  def map[B](f: A => B): State[S, B] = State(s => {
+    val (a, ss) = run(s)
+    (f(a), ss)
+  })
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = State(s => {
+    val (a, s1) = run(s)
+    val (b, s2) = sb.run(s1)
+    (f(a, b), s2)
+  })
+
+  //We could use flatMap to implement map and map2
+  def mapViaFlatMap[B](f: A => B): State[S, B] =
+    flatMap(a => unit((f(a), _)))
+
+  def map2ViaFlatMap[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap { a: A => sb.map { b: B => f(a, b) } }
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, ss) = run(s)
+    f(a).run(ss)
+  })
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  //Exercise 6.10
+  def unit[S, A](f: S => (A, S)): State[S, A] = State(f)
+
+  def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
+    fs.foldRight(unit((Nil, _)): State[S, List[A]])((a: State[S, A], b: State[S, List[A]]) => a.map2(b)(_ :: _))
+
+  //Reading the state
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  //Writing the state
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+}
 
 //Exercise 6.11
